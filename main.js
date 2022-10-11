526544b2571cfe86ffbb096812d2f33ce3495c38
lozanoojorgea <lozano.o.jorge.a@gmail.com>
2022-10-10 21:35:32 -0500

    rutas corregidas, email ofuscado

--- main.js
+++ main.js
@@ -3,3 +3,6 @@ var afterPrint = function() { $("details").removeAttr('open'); };
 if (window.matchMedia) { var mediaQueryList = window.matchMedia('print'); mediaQueryList.addListener(function(mql) { if (mql.matches) { beforePrint(); } else { afterPrint(); } }); }
 window.onbeforeprint = beforePrint;
 window.onafterprint = afterPrint;
+var encEmail = "bG96YW5vLm8uam9yZ2UuYUBnbWFpbC5jb20=";
+const form = document.getElementById("contact");
+form.setAttribute("href", "mailto:".concat(atob(encEmail)));
